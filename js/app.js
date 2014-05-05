da91e6b4b8ed428c4b40cde7f2676c547b871bf6
Adrain Lee <lxdiyun@gmail.com>
2014-05-05 17:08:10 +0800

    adding modal

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
 'use strict';
 
-var itlog_app = angular.module('itlogApp', ['itlogServices', 'ngTable']);
+var tableColumns  = [
+	{ title: 'url', field: 'url', visible: false },
+	{ title: '序号', field: 'number', visible: false },
+	{ title: '资产编号', field: 'sn', visible: true },
+	{ title: '分类号', field: 'catalog_id', visible: false },
+	{ title: '国标号', field: 'national_id', visible: false },
+	{ title: '资产名称', field: 'name', visible: true },
+	{ title: '资产型号', field: 'model', visible: true },
+	{ title: '资产规格', field: 'specification', visible: true },
+	{ title: '单价', field: 'price', visible: true },
+	{ title: '机身编号', field: 'sn2', visible: false },
+	{ title: '使用部门', field: 'department', visible: false },
+	{ title: '用户', field: 'user', visible: false },
+	{ title: '保管员', field: 'keeper', visible: false },
+	{ title: '单位责任人', field: 'officer', visible: false },
+	{ title: '状态', field: 'status', visible: false },
+	{ title: '地点', field: 'location', visible: false },
+	{ title: '购买日期', field: 'buy_date', visible: true },
+	{ title: '经费科目', field: 'funding_source', visible: false },
+	{ title: '入账日期', field: 'record_date', visible: true },
+	{ title: '国家', field: 'country', visible: false },
+	{ title: '供应商', field: 'provider', visible: false },
+	{ title: '折旧年限', field: 'depreciated_year', visible: false },
+	{ title: '已用年数', field: 'used_year', visible: false },
+	{ title: '折旧价格', field: 'depreciated_price', visible: false }
+]; 
+
+var itlog_app = angular.module('itlogApp', ['itlogServices', 'ngTable', 'ui.bootstrap']);
 
 
-itlog_app.controller('my_contorller', function($scope, Resource, ngTableParams) {
-	$scope.tableColumns = [
-		{ title: 'url', field: 'url', visible: false },
-		{ title: '序号', field: 'number', visible: false },
-		{ title: '资产编号', field: 'sn', visible: true },
-		{ title: '分类号', field: 'catalog_id', visible: false },
-		{ title: '国标号', field: 'national_id', visible: false },
-		{ title: '资产名称', field: 'name', visible: true },
-		{ title: '资产型号', field: 'model', visible: true },
-		{ title: '资产规格', field: 'specification', visible: true },
-		{ title: '单价', field: 'price', visible: true },
-		{ title: '机身编号', field: 'sn2', visible: false },
-		{ title: '使用部门', field: 'department', visible: false },
-		{ title: '用户', field: 'user', visible: false },
-		{ title: '保管员', field: 'keeper', visible: false },
-		{ title: '单位责任人', field: 'officer', visible: false },
-		{ title: '状态', field: 'status', visible: false },
-		{ title: '地点', field: 'location', visible: false },
-		{ title: '购买日期', field: 'buy_date', visible: true },
-		{ title: '经费科目', field: 'funding_source', visible: false },
-		{ title: '入账日期', field: 'record_date', visible: true },
-		{ title: '国家', field: 'country', visible: false },
-		{ title: '供应商', field: 'provider', visible: false },
-		{ title: '折旧年限', field: 'depreciated_year', visible: false },
-		{ title: '已用年数', field: 'used_year', visible: false },
-		{ title: '折旧价格', field: 'depreciated_price', visible: false }
-            ]; 
-
+itlog_app.controller('my_contorller', function($scope, $modal, Resource, ngTableParams) {
+	$scope.tableColumns = tableColumns;
 	$scope.tableParams = new ngTableParams({
 		page: 1,            // show first page
 		count: 10,          // count per page
@@ -75,4 +76,20 @@ itlog_app.controller('my_contorller', function($scope, Resource, ngTableParams) 
 			$scope.tableParams.reload();
 		}
 	});
+
+	$scope.open = function () {
+		var modalInstance = $modal.open({
+			templateUrl: 'detail.html',
+			controller: ModalInstanceCtrl,
+		});
+	};
 });
+
+var ModalInstanceCtrl = function($scope, $modalInstance) {
+
+	$scope.tableColumns = tableColumns;
+	$scope.ok = function () {
+		$modalInstance.close();
+	};
+
+};
--- js/main.js
+++ js/main.js
@@ -6,18 +6,20 @@ require.config({
 		'angular': '../bower_components/angular/angular.min',
 		'lodash': '../bower_components/lodash/dist/lodash.underscore.min',
 		'restangular': '../bower_components/restangular/dist/restangular.min',
-		'ngTable': '../bower_components/ng-table/ng-table'
+		'ngTable': '../bower_components/ng-table/ng-table',
+		'uiBootstrap': '../bower_components/angular-bootstrap/ui-bootstrap-tpls.min'
 
 	},
 	shim: {
 		'angular': { exports: 'angular' },
 		'restangular': { deps: ['angular', 'lodash'] },
 		'ngTable': { deps: ['angular'] },
+		'uiBootstrap': {deps: ['angular']},
 		'services': { deps: ['angular','restangular']},
-		'app': {deps: ['angular', 'services']}
+		'app': {deps: ['angular', 'services', 'ngTable', 'uiBootstrap']}
 	}
 });
 
-require(['angular', 'lodash', 'restangular', 'ngTable', 'services', 'app'], function (angular) {
+require(['angular', 'app'], function (angular) {
 	angular.bootstrap(document, ['itlogApp']);
 });
